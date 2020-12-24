5278f599fd34c2efe9868f82fe0b9fdcaf43a06d
wusung <twkoci@gmail.com>
2020-12-24 11:17:11 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,5 @@
+sudo apt-get install gnupg2
+wget https://repo.percona.com/apt/percona-release_latest.$(lsb_release -sc)_all.deb
+sudo dpkg -i percona-release_latest.$(lsb_release -sc)_all.deb
+sudo percona-release setup ps80
+sudo apt-get install percona-server-server
